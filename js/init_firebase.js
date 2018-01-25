36834f73ff7babdf58375cfb78c9eb13737255cc
RayPan <Ray0928066818@gmail.com>
2018-01-25 13:09:29 +0800

    Ray
    first

---
+++ js/init_firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA0_VTXxflyd-1jpLNChbjhfdQ2S8fXbFU",
+    authDomain: "chatroom-20180125.firebaseapp.com",
+    databaseURL: "https://chatroom-20180125.firebaseio.com",
+    projectId: "chatroom-20180125",
+    storageBucket: "chatroom-20180125.appspot.com",
+    messagingSenderId: "741969632429"
+};
+firebase.initializeApp(config);
