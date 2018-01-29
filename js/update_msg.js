a1f9fbd1362fa56236fcdd9378488443b5d90485
RayPan <Ray0928066818@gmail.com>
2018-01-29 10:44:44 +0800

    Ray
    新增 訊息發送時間
    視窗捲軸自動置底

--- js/update_msg.js
+++ js/update_msg.js
@@ -1,10 +1,29 @@
 var is_ie = (navigator.userAgent.match(/IE/i) != null);
 var is_firefox = (navigator.userAgent.match(/Firefox/i) != null);
-
 $(document).ready(function () {
     if (is_ie) {
         alert('當前瀏覽器不支援，請升級瀏覽器版本或使用其他瀏覽器開啟');
         return false;
+    }
+    function get_deadline() {
+        var deadline = new Date();
+        var hours = deadline.getHours();
+        var min = deadline.getMinutes();
+        var sec = deadline.getSeconds();
+
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+        if (min < 10) {
+            min = '0' + min;
+        }
+        if (sec < 10) {
+            sec = '0' + sec;
+        }
+
+        var str_deadline = deadline.getFullYear() + '/' + (deadline.getMonth() + 1) + '/' + deadline.getDate() +
+            ' ' + hours + '：' + min + '：' + sec;
+        return str_deadline;
     }
 
     var msg_ref = firebase.database().ref('/messages/');
@@ -12,29 +31,33 @@ $(document).ready(function () {
         msg_ref.once('value').then(function (snapshot) {
             var val = snapshot.val();
             var messages = '';
+            var text_box = document.getElementById('text_box');
             $.each(val, function (i, item) {
                 messages = messages + '<p>' + item.name + '：' + item.message + '</p>';
             });
             $('#text_box').html(messages);
+            text_box.scrollTop = text_box.scrollHeight;
         })
     }
 
     msg_ref.on('value', function (snapshot) {
         var val = snapshot.val();
-        console.log(val);
         var messages = '';
+        var text_box = document.getElementById('text_box');
         $.each(val, function (i, item) {
-            messages = messages + '<p>' + item.name + '：' + '<span style="color: ' + item.color + '">' + item.message + '</span>' + '</p>';
+            messages = messages + '<p>' + item.name + '：' + '<span style="color: ' + item.color + '">' + item.message + '</span><span class="msg_date">' + item.date + '</span></p>';
         });
         $('#text_box').html(messages);
+        text_box.scrollTop = text_box.scrollHeight;
     })
 
+    var text_box = $("#text_box");
     $('#send').on('click', function () {
-        var user = firebase.auth().currentUser;
         var msg = $('#msg').val();
         var name = $('#name').val();
         var color = $('#color').val();
-        msg_ref.push({ 'message': msg, 'name': name, 'color': color });
+        var date = get_deadline();
+        msg_ref.push({ 'message': msg, 'name': name, 'color': color, 'date': date });
     });
 
     $('#msg').focus(function (e) {
